63279574f148d5643137023d7b3e77f4fc1e13ef
Joseph Perla <noootreeeeall@**jperla.com**>
2012-02-06 17:34:27 -0500

    moved stuff around

--- model/data/viccidata/model/jsondata.py
+++ model/data/viccidata/model/jsondata.py
@@ -2,7 +2,18 @@
     jsondata helps you read data files encoded in json.
     Copyright (C) 2011 Joseph Perla
 
-    GNU Affero General Public License. See <http://www.gnu.org/licenses/>.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import json
 import numpy
